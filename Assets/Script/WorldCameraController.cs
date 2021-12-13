226b125348cf1c9b28c55fa36a1a05793e488ba6
Jenny Tran <puijanjenny.tran@students.fhnw.ch>
2021-12-13 14:37:03 +0100

    create logic to trigger event

--- Assets/Script/WorldCameraController.cs
+++ Assets/Script/WorldCameraController.cs
@@ -4,36 +4,73 @@ using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class WorldCameraController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera cam;
-    [SerializeField] private float speed;
-    public CinemachineTrackedDolly trackedDolly;
+    [SerializeField] private float speed = 2f;
 
+    CinemachineTrackedDolly trackedDolly;
     private float position;
-    private float horizontal;
+    private float pathLength;
+    private float minPos = 0;
+    private float maxPos;
 
-    public void Start()
+    private float horizontal;
+    private double checkTriggerPosition;
+
+    public GameObject GameObject; 
+
+    private void Start()
     {
         trackedDolly = cam.GetCinemachineComponent<CinemachineTrackedDolly>();
+        pathLength = trackedDolly.m_Path.PathLength;
+        maxPos = trackedDolly.m_Path.MaxPos;
     }
 
     private void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         position = trackedDolly.m_PathPosition;
+        position += horizontal * speed * Time.deltaTime;
+        
+        string posToString = position.ToString("R");
+        double posToDouble = Double.Parse(posToString);
+        checkTriggerPosition = Math.Round(posToDouble);
+        
+        trackedDolly.m_PathPosition = position;
+        position = Mathf.Clamp(position, minPos, maxPos);
 
-        if (Input.mouseScrollDelta.y == -1 || Input.GetKeyDown(KeyCode.DownArrow))
+        if (checkTriggerPosition == 1f)
         {
-            position -= horizontal * speed * Time.deltaTime;
-        } else if (Input.mouseScrollDelta.y == +1 || Input.GetKeyDown(KeyCode.UpArrow))
+            GameObject.SetActive(false);
+            Debug.Log("Track 1 + "  + checkTriggerPosition);
+        } else if (checkTriggerPosition == 3f)
         {
-            position += horizontal * speed * Time.deltaTime;
+            GameObject.SetActive(true);
+            Debug.Log("Track 3 + " + checkTriggerPosition);
         }
         
-        // position = Mathf.Clamp01(position);
-        trackedDolly.m_PathPosition = position;
+        
+        /*
+        if (position > trackedDolly.m_Path.MinPos && (Input.mouseScrollDelta.y == -1 || Input.GetKeyDown(KeyCode.DownArrow)) )
+        {
+            position -= horizontal * speed * Time.deltaTime;
+            trackedDolly.m_PathPosition = position;
+
+            Debug.Log("DownArrowKey detected." + position);
+        }
+        else if (position < trackedDolly.m_Path.MaxPos && (Input.mouseScrollDelta.y == +1 || Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+            position += horizontal * speed * Time.deltaTime;
+            trackedDolly.m_PathPosition = position;
+
+            Debug.Log("UpArrowKey detected." + position);
+        }
+        
+        */
+
     }
 }
 
